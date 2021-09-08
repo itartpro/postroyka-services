3b84f0adb0b22a458887fa91ba2fa5557735cd53
leo-at-itartpro <mail@itart.pro>
2021-09-08 14:36:16 +0300

    security update

---
+++ certs/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"time"
+)
+
+func isOlderThanOneYear(t time.Time) bool {
+	return time.Now().Sub(t) > 365*24*time.Hour
+}
+
+func main() {
+	var valid = true
+
+	files := []string{"ca.cert", "ca.key", "ca.srl", "jwtkey.pem", "jwtkey.pub", "service.csr", "service.key", "service.pem"}
+	for _, v := range files {
+		f, err := os.Stat(v)
+		if os.IsNotExist(err) {
+			valid = false
+		} else {
+			if f.Mode().IsRegular() {
+				if isOlderThanOneYear(f.ModTime()) {
+					valid = false
+				}
+			}
+		}
+	}
+
+	if valid != true {
+		cmd := exec.Command("sh", "newcerts.sh")
+		err := cmd.Run()
+		if err != nil {
+			println("Certs microservice error:", err)
+		} else {
+			println("Made new certs")
+		}
+	}
+}
--- certs/newcerts.sh
+++ certs/newcerts.sh
@@ -1,6 +1,7 @@
 # Install libraries needed for certs generation on alpine linux
-apk update && apk upgrade && apk add --no-cache bash procps drill git coreutils libidn curl &&
-apk add openssl && chmod +x newcerts.sh &&
+apk update && apk upgrade &&
+apk add --no-cache bash procps drill git coreutils libidn curl openssh-keygen openssl &&
+chmod +x newcerts.sh &&
 # Remove old certificates
 rm *.pem &&
 rm *.srl &&
@@ -8,17 +9,16 @@ rm *.csr &&
 rm *.key &&
 rm *.pub &&
 rm *.cert &&
-# Make simple aes256 pem, pub, key (for use with dgrijalva/jwt-go)
-# Replace $JWTKEY_PASS with your own password
-openssl genrsa -aes256 -passout pass:$JWTKEY_PASS -out jwtkey.pem 2048 &&
-openssl rsa -pubout -in jwtkey.pem -passin pass:$JWTKEY_PASS -pubout -out jwtkey.pub &&
-openssl pkey -in jwtkey.pem -passin pass:$JWTKEY_PASS -out jwtkey.key &&
+# Make simple aes256 pem, pub (for use with  golang-jwt/jwt )
+# https://gist.github.com/ygotthilf/baa58da5c3dd1f69fae9
+ssh-keygen -t rsa -P "" -b 2048 -m PEM -f jwtkey.pem &&
+openssl rsa -in jwtkey.pem -pubout -outform PEM -out jwtkey.pub &&
 # Make key, srl, cert, key using config in service.conf.
 # In my use case for use with credentials.NewServerTLSFromFile in Go, for gRPC communication
-openssl genrsa -out ca.key 4096 &&
-openssl req -new -x509 -key ca.key -sha256 -subj "/C=RU/ST=Kineshma/O=client.local" -days 3650 -out ca.cert &&
-openssl genrsa -out service.key 4096 &&
+openssl genrsa -out ca.key 2048 &&
+openssl req -new -x509 -key ca.key -sha256 -subj "/C=RU/ST=Kineshma/O=client.local" -days 365 -out ca.cert &&
+openssl genrsa -out service.key 2048 &&
 sleep 1 &&
 openssl req -new -key service.key -out service.csr -config certificate.conf &&
 sleep 1 &&
-openssl x509 -req -in service.csr -CA ca.cert -CAkey ca.key -CAcreateserial -out service.pem -days 3650 -sha256 -extfile certificate.conf -extensions req_ext
+openssl x509 -req -in service.csr -CA ca.cert -CAkey ca.key -CAcreateserial -out service.pem -days 365 -sha256 -extfile certificate.conf -extensions req_ext
